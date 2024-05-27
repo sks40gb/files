0aa1fb9054447b6f07d71345d0b5cb6b6ea13c5b
Sunil Kumar Singh <sks.256@gmail.com>
2024-05-28 01:05:16 +0530

    Update 001.sh

--- 001.sh
+++ 001.sh
@@ -9,7 +9,7 @@ fi
 # job arguments
 
 PROVIDER=$1
-PROVIDER_MATCH=$2
+PROVIDERS_MATCH=$2
 INSTRUMENTS_MATCH=$3
 RUN_MODE_=$4
 LOCK_PROVIDER=$5
@@ -20,7 +20,7 @@ STATUS=$8
 echo "current unix user: ${USER}"
 
 if [[ $LOCK_INSTRUMENT == "null" ]]; then
-  echo "################ LOCK snapshot not needed for $PROVIDER process"
+  echo "######################## LOCK snapshot not needed for $PROVIDER process"
   exit 0
 fi
 
@@ -30,9 +30,7 @@ KERBEROS_USER_PRINCIPAL=@spark.history.kerberos.principal@
 
 hdfs dfs -get @APP_STORAGE_URL@/glx/keytab/${USER}/${USER}.keytab
 
-IFS='#'
-arrProvider=($PROVIDER)
-unset IFS
+IFS='#'; arrProvider=($PROVIDER); unset IFS;
 IFS=','
 arrProvidersMatch=($PROVIDERS_MATCH)
 arrInstrumentsMatch=($INSTRUMENTS_MATCH)
@@ -48,7 +46,7 @@ for i in "${arrProvider[@]}"; do
 
     if [[ $STATUS == "LOCK" ]]; then
 
-      echo "############### Starting LOCK [ $i } snapshot "
+      echo "####################### Starting LOCK [ $i ] snapshot "
 
       if [[ $i == "LD_OPTION" ]] || [[ $i == "ALL" ]]; then
         echo "LOCK $OPTION_DATE option snapshot : delete _UNLOCK_STATIC and create _LOCK_static and _UNLOCK_STATIC"
@@ -75,16 +73,25 @@ for i in "${arrProvider[@]}"; do
 
     elif [[ $STATUS == "UNLOCK" ]]; then
 
-      echo "###################### Starting UNLOCK [ $i ] snapshot "
+      echo "######################## Starting UNLOCK [ $i ] snapshot "
 
-      if [[ $ == "LD_OPTION" ]] || [[ $i == "ALL" ]]; then
+      if [[ $i == "LD_OPTION" ]] || [[ $i == "ALL" ]]; then
 
         echo "UNLOCK $OPTION_DATE option snapshot  :  create _UNLOCK_STATIC and delete _LOCK_STATIC"
 
-        hadoop fs -rm -f $OPT_SNAP_PATH
-        hadoop fs - touchz $_FUT_SNAP_PATH/_LOCK_STATIC
+        hadoop fs -rm -f $OPT_SNAP_PATH/_LOCK_STATIC
+        hadoop fs - touchz $OPT_SNAP_PATH/_UNLOCK_STATIC
         if [ "$?" -n "0" ]; then
           exit -1
+        fi
+      fi
+      if [[ $i == "LD_FUTURE" ]] || [[ $i == "ALL" ]]; then
+        echo "UNLOCK $FUTURE_DATE future snapshot : create _UNLOCK_STATIC and delete _LOCK_STATIC"
+
+          hadoop fs -rm -f $FUT_SNAP_PATH/_LOCK_STATIC
+          hadoop fs -touchz $FUT_SNAP_PATH/_UNLOCK_STATIC
+          if [ "$?" -ne "0" ]; then
+            exit -1
         fi
       fi
     fi
@@ -98,13 +105,13 @@ for i in "${arrProvidersMatch[@]}"; do
   if [[ $LOCK_PROVIDER == *"|$i|"* ]] || [[ $LOCK_DELTA == *"|$i|"* ]]; then
     if [[ $STATUS == "LOCK" ]]; then
 
-      echo "###############################Starting LOCK [ $i ] snapshot "
+      echo "#####################################Starting LOCK [ $i ] snapshot "
 
       if [[ $INSTRUMENTS_MATCH =~ "OPTION" ]]; then
         echo "LOCK $OPTION_DATE option snapshot : check if it not LOCK yet and delete _UNLOCK_STATIC and create _LOCK_STATIC"
 
         if [[ $LOCK_PROVIDER == *"|$i|"* ]]; then
-          hadoop ts -rm -f $OPT_SNAP_PATH/_UNLOCK_STATIC
+          hadoop fs -rm -f $OPT_SNAP_PATH/_UNLOCK_STATIC
           hadoop fs -touchz $OPT_SNAP_PATH/_LOCK_STATIC
           if [ "$?" -ne "0" ]; then
             exit -1
@@ -130,13 +137,13 @@ for i in "${arrProvidersMatch[@]}"; do
 
     elif [[ $STATUS == "UNLOCK" ]]; then
 
-      echo "######################### Starting UNLOCK [ $i ] snapshot "
+      echo "########################## Starting UNLOCK [ $i ] snapshot "
 
       if [[ $INSTRUMENTS_MATCH =~ "OPTION" ]]; then
         echo "UNLOCK $OPTION_DATE option snapshot : create _UNLOCK_STATIC and delete _LOCK_STATIC"
 
         if [[ $LOCK_PROVIDER == *"|$i|"* ]]; then
-          hadoop fs -rm -f $OP_SNAP_PATH/$OPTION_DATE/_LOCK_STATIC
+          hadoop fs -rm -f $OPT_SNAP_PATH/$OPTION_DATE/_LOCK_STATIC
           hadoop fs -touchz $OPT_SNAP_PATH/$OPTION_DATE/_UNLOCK_STATIC
           if [ "$?" -ne "0" ]; then
             exit -1
@@ -169,4 +176,4 @@ for i in "${arrProvidersMatch[@]}"; do
   fi
 
 done
-exti $?
+exit $?
